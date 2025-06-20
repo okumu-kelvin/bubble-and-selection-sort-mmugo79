c4f981b3ac5e72e09d6459b868c08c9671ad533e
mmugo79 <mugomoses506@gmail.com>
2025-06-20 14:01:37 +0300

    bubble

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,12 @@
-def bubble_sort(unsorted_list):
+def bubble_sort(data):
     # TODO: Implement bubble sort
-    pass
+    length = len(data)
+    for pass_num in range(length -1,0,-1):
+        for i in range(pass_num):
+            if data[i]>data[i + 1]:
+                #manual swap without tuple unpacking
+                temp = data[i]
+                data[i] = data[i + 1]
+                data[i + 1] = temp
+    return data            
+
--- selection_sort/test_selection_sort.py
+++ selection_sort/test_selection_sort.py
@@ -2,9 +2,12 @@ from selection_sort import selection_sort
 
 def test_sorted():
     assert selection_sort([1, 2, 3]) == [1, 2, 3]
+    print("test_sorted passed")
 
 def test_reverse():
     assert selection_sort([3, 2, 1]) == [1, 2, 3]
+    print("test_reverse passed")
 
 def test_duplicates():
     assert selection_sort([29, 10, 14, 37, 14]) == [10, 14, 14, 29, 37]
+    print("test_duplicates passed")
